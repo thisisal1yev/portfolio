f4dd76229ba1d059e1c54d2f3b6ada798ca84f3c
thisisal1ev <aaalievvv1@gmail.com>
2025-06-09 11:16:31 +0500

    feat: add SVG logos for various libraries and frameworks; create Home component

---
+++ src/app/index.tsx
@@ -0,0 +1,7 @@
+export function Home() {
+  return (
+    <main className='relative flex h-min w-full flex-none flex-nowrap items-start justify-center overflow-hidden'>
+      Hello world!
+    </main>
+  )
+}
