082b30a6f7639b53b2159766459989a04297baf1
thisisal1ev <aaalievvv1@gmail.com>
2025-06-14 10:37:52 +0500

    feat: implement Link component and update Home component with new layout and links

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,7 +1,54 @@
+import { Link } from '../shared/components/Link'
+
 export function Home() {
   return (
-    <main className='relative flex h-min w-full flex-none flex-nowrap items-start justify-center overflow-hidden'>
-      Hello world!
+    <main className='relative container flex h-min min-h-screen min-w-full flex-none flex-nowrap overflow-hidden px-15 py-5 antialiased'>
+      <div className='flex-3/5'>Right side</div>
+
+      <div className='flex-2/5'>
+        <div className='fixed top-0 right-15 z-10 flex min-h-screen w-1/3 flex-col py-5'>
+          <div className='flex items-center justify-between'>
+            <span>thisisaliyev®, v0.1</span>
+
+            <Link
+              label='тг.'
+              href='https://t.me/thisisaliyev'
+            >
+              @thisisaliyev
+            </Link>
+          </div>
+
+          <div className='flex h-auto grow flex-col items-start justify-center space-y-10'>
+            <div className='h-86 -rotate-4 rounded-2xl border-2 border-dashed p-5 py-5'>
+              <p className='text-7xl font-semibold'>
+                Здесь могла быть моя фотография
+              </p>
+            </div>
+
+            <h1 className='dark:text-purple ml-10 text-2xl leading-7 font-medium text-purple-800'>
+              Фронтенд разработчик
+            </h1>
+          </div>
+
+          <div>
+            <div className='flex items-center justify-between'>
+              <Link
+                label='т.'
+                href='tel:+998950490473'
+              >
+                +998 (95) 049 04 73
+              </Link>
+
+              <Link
+                label='e.'
+                href='mailto:aaalievvv1@gmail.com'
+              >
+                aaalievvv1@gmail.com
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
     </main>
   )
 }
---
+++ src/shared/components/Link.tsx
@@ -0,0 +1,26 @@
+import { cn } from '../lib/utils'
+
+export function Link({
+  children,
+  label,
+  href,
+  className,
+}: Readonly<{
+  children: React.ReactNode
+  href: string
+  label: string
+  className?: string
+}>) {
+  return (
+    <p className={cn('flex items-center gap-2', className)}>
+      <span>{label}&nbsp;</span>
+
+      <a
+        href={href}
+        className='underline-offset-4 hover:underline'
+      >
+        {children}
+      </a>
+    </p>
+  )
+}
